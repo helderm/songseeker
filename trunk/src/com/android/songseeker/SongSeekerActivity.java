364696326073d66cd372dff50277233a450f05d1
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-07-28 21:24:40 +0000

    Project created

---
+++ trunk/src/com/android/songseeker/SongSeekerActivity.java
@@ -0,0 +1,13 @@
+package com.android.songseeker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SongSeekerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
