4b353a16f2557c459d22e7da301c8c41d2177c7e
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-08-02 02:19:07 +0000

    - Added call to Echo Nest API - Added first draw of the serach result screen

--- trunk/src/com/android/songseeker/SongSeekerActivity.java
+++ trunk/src/com/android/songseeker/SongSeekerActivity.java
@@ -1,48 +1,73 @@
 package com.android.songseeker;
 
-//import com.echonest.api.v4.EchoNestAPI;
-//import com.echonest.api.v4.EchoNestException;
-//import com.echonest.api.v4.Playlist;
-//import com.echonest.api.v4.PlaylistParams;
+import java.util.Random;
+
+import com.echonest.api.v4.EchoNestAPI;
+import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.Playlist;
+import com.echonest.api.v4.PlaylistParams;
+import com.echonest.api.v4.Song;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SongSeekerActivity extends Activity {
    	
-	//EchoNestAPI en;
+	EchoNestAPI en;
+	Button search;
+	Playlist playlist;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //setContentView(R.layout.search);
         
-        ///System.setProperty("ECHO_NEST_API_KEY", "OKF60XQ3DSLHDO9CX");
+        //init Echo Nest API object
+        en = new EchoNestAPI("OKF60XQ3DSLHDO9CX");		
         
-        //try {
-        //	EchoNestAPI en = new EchoNestAPI();
-		//} catch (EchoNestException e1) {
-			// TODO Auto-generated catch block
-		//	e1.printStackTrace();
-		//}
+        //search = (Button)findViewById(R.id.search_but);
+        //search.setOnClickListener(new View.OnClickListener() {
+        //    public void onClick(View v) {
+        //    	createSearchActivity();
+        //    }
+        //}); 
         
-        //PlaylistParams p = new PlaylistParams(); 
-        //p.addArtist("Weezer");
-        //p.setResults(10);   
-        //try {
-		//	Playlist playlist = en.createStaticPlaylist(p);
-		//} catch (EchoNestException e) {
-			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//}
-
-          
-        
+//        PlaylistParams p = new PlaylistParams(); 
+//        p.addArtist("Eric Clapton");
+//        p.setResults(20);   
+//        try {
+//			playlist = en.createStaticPlaylist(p);
+//		} catch (EchoNestException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}          
+//			
+//		LinearLayout l = (LinearLayout) findViewById(R.id.search_layout);
+//        LayoutInflater linflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//		for(Song song : playlist.getSongs()){
+//			
+//            View myView = linflater.inflate(R.layout.song_search, null);
+//
+//            TextView t = (TextView) myView.findViewById(R.id.song_info);
+//
+//            //Change name dynamically
+//            t.setText(("Artist: " + song.getArtistName() + " - Song: "+ song.getReleaseName()).toString());
+//            l.addView(myView);
+//			
+//		}
+		
     }
     
     @Override
@@ -74,4 +99,10 @@ public class SongSeekerActivity extends Activity {
     	Intent i = new Intent(SongSeekerActivity.this, PlaylistOptionsActivity.class);
         startActivity(i);	
     }
+    
+    private void createSearchActivity(){
+    	Intent i = new Intent(SongSeekerActivity.this, PlaylistOptionsActivity.class);
+        startActivity(i);    	
+    }
+        
 }
