444f0e9cb107a56c15eb0673a910d37bc332f7ab
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-08-03 21:03:17 +0000

    - Added first draw of the recommended songs being printed

--- trunk/src/com/android/songseeker/RecSongsActivity.java
+++ trunk/src/com/android/songseeker/RecSongsActivity.java
@@ -1,38 +1,75 @@
 package com.android.songseeker;
 
+import java.util.List;
+
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Playlist;
 import com.echonest.api.v4.PlaylistParams;
+import com.echonest.api.v4.Song;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RecSongsActivity extends Activity {
 
+	private List<Song> recSongs;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		Intent intent;
-		PlaylistParams plParams;
-		Playlist playlist;
-		EchoNestAPI en;
-		int numArtists;
+
 		
 		super.onCreate(savedInstanceState);
 	    
 	    setContentView(R.layout.rec_songs_list);
 	
-	    intent = getIntent();
-	    numArtists = intent.getIntExtra("num_artist", -1);
+	    getRecommendedSongs();
+	    
+	    printRecommendedSongs();
+		
+	    //get the input from the
+
+	}
+
+	private void printRecommendedSongs() {
+		LinearLayout l = (LinearLayout) findViewById(R.id.rec_songs_list_layout);
+		LayoutInflater linflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		
+		for(Song song : recSongs){
+			
+          View myView = linflater.inflate(R.layout.rec_song, null);
+
+          TextView t = (TextView) myView.findViewById(R.id.song_info);
+
+          //Change name dynamically
+          t.setText(("Artist: " + song.getArtistName() + " - Song: "+ song.getReleaseName()).toString());
+          l.addView(myView);			
+		}
+		
+	}
+
+	private void getRecommendedSongs() {
+		PlaylistParams plParams;
+		Playlist playlist;
+		EchoNestAPI en;
+		int numArtists;
+		
+	    numArtists = getIntent().getIntExtra("num_artist", -1);
 	    
 	    plParams = new PlaylistParams();
 	    plParams.setResults(20);   
 
 	    for(int i=0; i<numArtists; i++){
-	    	String str = intent.getStringExtra("artist"+i);
+	    	String str = getIntent().getStringExtra("artist"+i);
 	    	if(str.equals(null)){
 	    		Log.w("RecSongsActivity", "ignoring null artist ["+i+"]");
 	    		continue;
@@ -46,12 +83,12 @@ public class RecSongsActivity extends Activity {
 	    try {
 			playlist = en.createStaticPlaylist(plParams);
 		} catch (EchoNestException e) {
-			// TODO Auto-generated catch block
+			// TODO Add an error popup screen to the user and go back to the previous screen
 			Log.e("RecSongsActivity", "createStaticPlaylist failed!", e);
+			return;
 		}
 	    
-	    //get the input from the
-	    // TODO Auto-generated method stub
+		recSongs = playlist.getSongs();
 	}
 
 }
