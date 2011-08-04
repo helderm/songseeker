bf125c4e1eb0428499a008914388fe5b101d9ede
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-08-04 22:45:36 +0000

    - Added some playlist options

--- trunk/src/com/android/songseeker/RecSongsActivity.java
+++ trunk/src/com/android/songseeker/RecSongsActivity.java
@@ -6,17 +6,16 @@ import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Playlist;
 import com.echonest.api.v4.PlaylistParams;
+import com.echonest.api.v4.PlaylistParams.PlaylistType;
 import com.echonest.api.v4.Song;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class RecSongsActivity extends Activity {
@@ -52,7 +51,7 @@ public class RecSongsActivity extends Activity {
           TextView t = (TextView) myView.findViewById(R.id.song_info);
 
           //Change name dynamically
-          t.setText(("Artist: " + song.getArtistName() + " - Song: "+ song.getReleaseName()).toString());
+          t.setText((song.getReleaseName()).toString() + " - " + song.getArtistName());
           l.addView(myView);			
 		}
 		
@@ -68,7 +67,12 @@ public class RecSongsActivity extends Activity {
 	    
 	    plParams = new PlaylistParams();
 	    plParams.setResults(20);   
-
+	    plParams.setType(PlaylistType.ARTIST_RADIO);
+	    plParams.setVariety(0.1f);
+	    plParams.setMaxEnergy(Settings.pl_energy/100);
+	    //TODO add other options
+	    
+	    
 	    for(int i=0; i<numArtists; i++){
 	    	String str = getIntent().getStringExtra("artist"+i);
 	    	if(str.equals(null)){
@@ -79,11 +83,12 @@ public class RecSongsActivity extends Activity {
 	    	plParams.addArtist(str);	    	
 	    	
 	    }
-	    
+	    	    
+
 	    en = new EchoNestAPI("OKF60XQ3DSLHDO9CX");
 	    try {
 			playlist = en.createStaticPlaylist(plParams);
-		} catch (EchoNestException e) {
+	    } catch (EchoNestException e) {
 			// TODO Add an error popup screen to the user and go back to the previous screen
 			Log.e("RecSongsActivity", "createStaticPlaylist failed!", e);
 			return;
