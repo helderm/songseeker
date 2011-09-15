8f038ae4913a5b5bed9d7694b1eee86224f795f2
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-09-15 21:37:57 +0000

    Missed file

--- trunk/src/com/android/songseeker/comm/youtube/Item.java
+++ trunk/src/com/android/songseeker/comm/youtube/Item.java
@@ -1,5 +1,7 @@
 package com.android.songseeker.comm.youtube;
 
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.client.util.Key;
 
@@ -13,4 +15,9 @@ public class Item {
   
   @Key
   public String id;
+  
+  public JsonHttpContent toContent(JsonFactory jsonFactory) {
+	  JsonHttpContent result = new JsonHttpContent(jsonFactory, this);
+	  return result;
+  }
 }
