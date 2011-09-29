86305f62daa719cd06a8ef0791cc167fd6a2b0e3
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-09-29 03:37:21 +0000

    - Added ReleaseInfo activity and xml

---
+++ trunk/src/com/android/songseeker/activity/ReleaseInfoActivity.java
@@ -0,0 +1,16 @@
+package com.android.songseeker.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReleaseInfoActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
