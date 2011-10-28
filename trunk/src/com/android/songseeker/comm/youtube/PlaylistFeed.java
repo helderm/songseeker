0c7355fc4c6b10924fb773327305b6dc8b14bb74
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-10-28 21:30:24 +0000

    - Finished YouTube playlist creation - Added button Watch to Song/ArtistInfo screens - Removed old google-data classes

--- trunk/src/com/android/songseeker/comm/youtube/PlaylistFeed.java
+++
@@ -1,5 +0,0 @@
-package com.android.songseeker.comm.youtube;
-
-public class PlaylistFeed extends Feed<Playlist>{
-
-}
--- trunk/src/com/android/songseeker/comm/youtube/VideoFeed.java
+++
@@ -1,4 +0,0 @@
-package com.android.songseeker.comm.youtube;
-
-public class VideoFeed extends Feed<Video> {
-}
