8a5cc255322b0330895fb60b28dc54acd8a56858
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2011-11-07 19:34:48 +0000

    - Added androidicon's icons to menu - Added EN logo to main screen

--- trunk/src/com/android/songseeker/activity/SimilarArtistsActivity.java
+++ trunk/src/com/android/songseeker/activity/SimilarArtistsActivity.java
@@ -168,7 +168,7 @@ public class SimilarArtistsActivity extends ListActivity {
 				holder.line.setText(artist.name);			
 
 				try{
-					ImageLoader.getLoader(getCacheDir()).DisplayImage(artist.image, holder.coverArt, R.drawable.artist);
+					ImageLoader.getLoader(getCacheDir()).DisplayImage(artist.image, holder.coverArt, R.drawable.ic_menu_disc);
 				}catch(IndexOutOfBoundsException e){
 					Log.w(Util.APP, "Unable to fetch the release image from Echo Nest!");
 				}
