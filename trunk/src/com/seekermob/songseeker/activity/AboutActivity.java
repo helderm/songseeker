da67364d20acf29be5650ef26d2c32ba2ab51ec1
heldergaray <heldergaray@8d9a5579-8710-4b04-bc38-e067a382753f>
2012-01-03 01:59:03 +0000

    - Improved AboutActivity screen

--- trunk/src/com/seekermob/songseeker/activity/AboutActivity.java
+++ trunk/src/com/seekermob/songseeker/activity/AboutActivity.java
@@ -3,16 +3,57 @@ package com.seekermob.songseeker.activity;
 import com.seekermob.songseeker.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	
-	    setContentView(R.layout.about);
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.about);
+
+		ImageView logo = (ImageView) findViewById(R.id.echonest_logo);
+		logo.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://the.echonest.com/"));
+				intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
+		
+		logo = (ImageView) findViewById(R.id.sevendigital_logo);
+		logo.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.7digital.com/"));
+				intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
+		
+		logo = (ImageView) findViewById(R.id.designer_logo);
+		logo.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.lucassanchez.com.br/"));
+				intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
+		
+		logo = (ImageView) findViewById(R.id.androidicons_logo);
+		logo.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.androidicons.com/"));
+				intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});		
+
 	}
 
 }
